9693e5f2ce9efaa6c16d519f154a5e41ca1ce80f
Bart Moeneclaey <bart.moeneclaey@student.ap.be>
2018-09-03 14:34:24 +0200

    Oprdracht 2 toegevoegd
    Express

---
+++ Express/express-eightball/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Listening on port 3000');
+});
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
